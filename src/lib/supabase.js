7caec342a12b2f7c86f6669e6e6fe8a245bb9379
Ibrahim Karimov <ibrahimkarimov@hotmail.com>
2026-05-19 17:02:57 +0400

    fixed

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  'https://kvgqriwbpcpgpcwyexte.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2Z3FyaXdicGNwZ3Bjd3lleHRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0MTQ1MTksImV4cCI6MjA5Mzk5MDUxOX0.fRR2iziPVHYGjPki7nZO8fo9FvH28QWSi7Y7Yk67QJY'
 )
